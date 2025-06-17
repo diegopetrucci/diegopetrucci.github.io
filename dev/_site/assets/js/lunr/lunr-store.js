b8945d01f349fd357700df9724d6548a0558dd27
Diego Petrucci <baulei@icloud.com>
2025-06-17 10:55:21 +0100

    Remove /dev

--- dev/_site/assets/js/lunr/lunr-store.js
+++
@@ -1,7 +0,0 @@
-var store = [{
-        "title": "Welcome to My Dev Blog",
-        "excerpt":"Welcome to my development blog! This is where I’ll be sharing thoughts on iOS development, Swift, and other tech topics that interest me. As an iOS engineer at Numan, I work on making healthcare more accessible for men. When I’m not coding, you can find me dancing or bouldering around...","categories": ["blog"],
-        "tags": ["welcome","ios","development"],
-        "url": "/dev/blog/2024/12/16/welcome-to-my-dev-blog.html",
-        "teaser": null
-      }]
